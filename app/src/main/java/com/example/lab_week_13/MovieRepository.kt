797aac4f7e9ef6ca42114b3fad6c48968eeebbd1
Manuel Manatap <manuelp.sianturi@gmail.com>
2025-12-16 21:18:47 +0700

    Commit No. 2 – update to Room

---
+++ app/src/main/java/com/example/lab_week_13/MovieRepository.kt
@@ -0,0 +1,38 @@
+package com.example.lab_week_13
+
+import com.example.lab_week_13.api.MovieService
+import com.example.lab_week_13.database.MovieDao
+import com.example.lab_week_13.database.MovieDatabase
+import com.example.lab_week_13.model.Movie
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+
+class MovieRepository(
+    private val movieService: MovieService,
+    private val movieDatabase: MovieDatabase
+) {
+
+    private val apiKey = "9354f0ce5aeda8ea7fde7d4b058b7a44"
+
+    fun fetchMovies(): Flow<List<Movie>> {
+        return flow {
+
+            val movieDao: MovieDao = movieDatabase.movieDao()
+            val savedMovies = movieDao.getMovies()
+
+
+            if (savedMovies.isEmpty()) {
+                val movies = movieService.getPopularMovies(apiKey).results
+
+                movieDao.addMovies(movies)
+
+                emit(movies)
+            } else {
+
+                emit(savedMovies)
+            }
+        }.flowOn(Dispatchers.IO)
+    }
+}
