dd2aaa9af7bd70ad4289fb0ed8eb810f6b5dbc98
Manuel Manatap <manuelp.sianturi@gmail.com>
2025-12-16 21:31:02 +0700

    Commit No. 3 – update to WorkManager

--- app/src/main/java/com/example/lab_week_13/MovieApplication.kt
+++ app/src/main/java/com/example/lab_week_13/MovieApplication.kt
@@ -1,10 +1,15 @@
 package com.example.lab_week_13
 
 import android.app.Application
+import androidx.work.Constraints
+import androidx.work.NetworkType
+import androidx.work.PeriodicWorkRequest
+import androidx.work.WorkManager
 import com.example.lab_week_13.api.MovieService
 import com.example.lab_week_13.database.MovieDatabase
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
 
 class MovieApplication : Application() {
 
@@ -12,6 +17,7 @@ class MovieApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+
 
         val retrofit = Retrofit.Builder()
             .baseUrl("https://api.themoviedb.org/3/")
@@ -27,5 +33,23 @@ class MovieApplication : Application() {
 
 
         movieRepository = MovieRepository(movieService, movieDatabase)
+
+
+        val constraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .build()
+
+
+        val workRequest = PeriodicWorkRequest.Builder(
+            MovieWorker::class.java,
+            1,
+            TimeUnit.HOURS
+        )
+            .setConstraints(constraints)
+            .addTag("movie-work")
+            .build()
+
+
+        WorkManager.getInstance(applicationContext).enqueue(workRequest)
     }
 }
